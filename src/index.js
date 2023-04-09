7414cbf184204b2f919f65a33a53e6e40d2714fe
VladLys0v <kirdgach@gmail.com>
2023-04-09 19:39:59 +0200

    +possibility to refresh the page with <HashRouter>

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
@@ -10,12 +10,12 @@ import Vocabulary from './components/vocabulary/Vocabulary';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route exact path="/" element={<App />} />
 
       <Route exact path="/:language" element={<Vocabulary language="{language}" />} />
 
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
