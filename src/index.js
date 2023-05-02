d3f6b055eeec1213a57fa296caa8699cbbcd8c38
VladLys0v <kirdgach@gmail.com>
2023-05-03 01:04:19 +0200

    index code grooming

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { HashRouter, Route, Routes } from 'react-router-dom';
-
 import App from './App';
 import './index.css';
-
 import { createRoot } from 'react-dom/client';
 import Vocabulary from './components/vocabulary/Vocabulary';
 import MemoryCards from 'C:/Users/Vlad/Desktop/langarts/src/containers/memoryCards/MemoryCards.jsx';
@@ -14,11 +12,8 @@ root.render(
   <HashRouter>
     <Routes>
       <Route exact path="/" element={<App />} />
-
-      <Route exact path="/:vocabulary" element={<Vocabulary />} />
-
-      <Route exact path="/:memory-cards" element={<MemoryCards />} />
-
+      <Route exact path="/vocabulary" element={<Vocabulary />} />
+      <Route exact path="/memory-cards" element={<MemoryCards />} />
     </Routes>
   </HashRouter>
 );
