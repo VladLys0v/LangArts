1f57dbd322e6110ea4c3a3617e68931751018b3e
VladLys0v <kirdgach@gmail.com>
2023-05-09 14:53:47 +0200

    css grooming for MemoryCards

--- src/containers/memoryCards/MemoryCards.jsx
+++ src/containers/memoryCards/MemoryCards.jsx
@@ -162,6 +162,7 @@ const MemoryCards = ({ showMemoryCards, setShowMemoryCards, language, language2 
         )}
         {showWords && words.length > 0 && (
           <div className = "langarts__memoryCards__afterCountdown">
+          
             <div className ="langarts__memoryCards__taskWord">
             <div className ="langarts__memoryCards__previousWord">
              {currentIndex > 0 && (
