9b586bf896049e77aa36b608ab50738061cbd3b7
VladLys0v <kirdgach@gmail.com>
2023-05-17 22:03:26 +0200

    positioned language swap in the bottom

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
       <div className="langarts__header-content">
         <h1 className="gradient__text">Learn New Languages with Ease</h1>
         <p>We all know how difficult it can be to start learning a new language. That's why our application makes it easy and fun to improve your language skills and vocabulary.</p>
-        <button className="cta-button">Get Started</button>
+        <button className="cta-button">More{'>'} </button>
       </div>
     </div>
   )
