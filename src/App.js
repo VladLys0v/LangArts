4b55f35dc7291b9876163e73cb01e7d2f306fbed
VladLys0v <kirdgach@gmail.com>
2023-05-21 17:01:32 +0200

    +new picture and a slide functionality for cta button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import { Footer, Blog, Topics, Features, Header } from './containers'
 import { Navbar } from './components'
 import './App.css'
 
 const App = () => {
+  const [showNewPicture, setShowNewPicture] = useState(false);
+
+  const handleCTAButtonClick = () => {
+    setShowNewPicture(true);
+  };
+
   return (
     <div className="App">
-      <div className="gradient__pic">     
+      <div className={`gradient__pic ${showNewPicture ? 'slide-left' : ''}`}>  
       <Navbar />
-      <Header />
+      <Header handleCTAButtonClick={handleCTAButtonClick} />
       </div>
       <Topics id="topics" />
       <Features />
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import './header.css'
 
-const Header = () => {
+const Header = ({handleCTAButtonClick}) => {
   return (
     <div className="langarts__header section__padding" id="home">
       <div className="langarts__header-content">
         <h1 className="gradient__text">Learn New Languages with Ease</h1>
         <p>We all know how difficult it can be to start learning a new language. That's why our application makes it easy and fun to improve your language skills and vocabulary.</p>
-        <button className="cta-button">More{'>'} </button>
+        <button className="cta-button"onClick={handleCTAButtonClick}>More{'>'} </button>
       </div>
     </div>
   )
